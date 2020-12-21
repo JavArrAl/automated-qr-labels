cb094f1d153c91f62c93a634495cbdf92dbd088e
Aphi29 <jarranz@kth.se>
2020-12-21 22:17:06 +0000

    Cleaning error dict

--- variableFile.py
+++ variableFile.py
@@ -3,7 +3,7 @@ AI = {
     '(02)': ['Job Status', 'Job_Status'],
     '(10)': ['Creation Date', 'Creation_Date'],
     '(11)': ['Call_Date', 'Call Date'],
-    '(11)': ['Close_Date', 'Close Date'],
+    '(12)': ['Close_Date', 'Close Date'],
     '(13)': ['Work_End_Date', 'Work End Date','Service_Date', 'Service Date'],
     '(21)': ['Serial_Number', ' Serial Number', 'S/N', 'S No', 'S_No', 'Serial No', 'Serial_No'],
     '(22)': ['DS Serial No', 'DS_Serial_No', ' Docking Station', 'Docking_Station', 'Docking Station SN', 'Docking_Station_SN', 'DS_SN', 'DS SN', 'DS SNo', 'DS_SNo'],
