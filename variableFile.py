7790ee94a53cc94d24b46d4ef6a10b5cdb165219
Aphi29 <jarranz@kth.se>
2021-01-02 16:29:46 +0000

    First Ready label implementation

--- variableFile.py
+++ variableFile.py
@@ -7,5 +7,7 @@ AI = {
     '(13)': ['Work_End_Date', 'Work End Date','Service_Date', 'Service Date'],
     '(21)': ['Serial_Number', ' Serial Number', 'S/N', 'S No', 'S_No', 'Serial No', 'Serial_No'],
     '(22)': ['DS Serial No', 'DS_Serial_No', ' Docking Station', 'Docking_Station', 'Docking Station SN', 'Docking_Station_SN', 'DS_SN', 'DS SN', 'DS SNo', 'DS_SNo'],
-    '(30)': ['Equipment_Model', 'Equipment Model', 'Model', 'Device Type', 'Device_Type']
+    '(30)': ['Equipment_Model', 'Equipment Model', 'Model', 'Device Type', 'Device_Type'],
+    '(90)': ['Settings', 'Configuration'],
+    '(91)': ['Consumables']
 }
